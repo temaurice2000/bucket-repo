1aa099f1e11b423053c9fba42154b5d2b6eaba00
Mauri <temaurice2000@yahoo.fr>
2022-04-04 22:31:17 -0400

    new

--- bucket.sh
+++ bucket.sh
@@ -3,7 +3,7 @@
 echo "This scritp will create a bucket after a push in the remote repo"
 
 # first the script will check wether the bucket exists already
-Date=$(date | awk '{print $4}')
+Date=$(date | awk '{print $4}' | sed 's/:/-/g')
 exist=$(aws s3api head-bucket --bucket new-maurice-bucket-2022 2>&1 || true)
 if [ -z "$exist" ]; then 
    echo "This bucket exists"
