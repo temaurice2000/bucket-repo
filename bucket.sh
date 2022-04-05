ae10e8137a84ad9aa1df33076b1a7b88719ac7ad
Mauri <temaurice2000@yahoo.fr>
2022-04-04 22:04:30 -0400

    third commit

--- bucket.sh
+++ bucket.sh
@@ -4,11 +4,11 @@ echo "This scritp will create a bucket after a push in the remote repo"
 
 # first the script will check wether the bucket exists already
 
-exist=$(aws s3api head-bucket --bucket new-maurice-bucket-2024 2>&1 || true)
+exist=$(aws s3api head-bucket --bucket new-maurice-bucket-2022 2>&1 || true)
 if [ -z "$exist" ]; then 
    echo "This bucket exists"
 else
-    echo "This bucket will be created with the name new-maurice-bucket-2024"
+    echo "This bucket will be created with the name new-maurice-bucket-2022"
     aws s3api create-bucket --bucket new-maurice-bucket-2024 --create-bucket-configuration LocationConstraint=us-west-2
 fi 
 echo "Thanks"
